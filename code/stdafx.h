f30f348209f10974573968dcf74fb48cf4209569
[motoki-tmu] <[sogabe-motoki@ed.tmu.ac.jp]>
2025-10-06 21:13:27 +0900

    Changes to be committed: 	new file:   code/CNEllipseDetector.cpp 	new file:   code/CNEllipseDetector.h 	new file:   code/Main.cpp 	new file:   code/common.cpp 	new file:   code/common.h 	new file:   code/dirent.h 	new file:   code/stdafx.h 	new file:   code/tools.cpp 	new file:   code/tools.h
     Changes not staged for commit:
    	modified:   code/stdafx.h
    	modified:   code/tools.cpp
    	modified:   code/tools.h
    
     Untracked files:
    	code/cned
    	code/result/
    	image/

---
+++ code/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/highgui/highgui_c.h>
+#include <opencv2/opencv.hpp>
+
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types_c.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include <sys/stat.h> 
+#include <dirent.h>
